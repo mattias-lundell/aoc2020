0f206b1fbd721b262461a7a4ba526c05d85d7b81
Mattias Lundell <mattias@fishbrain.com>
2020-12-09 14:28:50 +0100

    clean day 9

--- 09.py
+++ 09.py
@@ -1,32 +1,22 @@
 from itertools import combinations
+from helpers import ints
 
 
 def part1(data):
-    x = 5 if len(data) < 100 else 25
-    for i in range(x, len(data)):
-        prev = data[i - x:i]
-        curr = int(data[i])
-        sums = set([int(a) + int(b) for (a, b) in combinations(prev, 2)])
-
-        if curr not in sums:
-            return curr
+    return find(ints(data))
 
 
 def find(data):
     x = 5 if len(data) < 100 else 25
     for i in range(x, len(data)):
-        prev = data[i - x:i]
-        curr = int(data[i])
-        sums = set([int(a) + int(b) for (a, b) in combinations(prev, 2)])
-
-        if curr not in sums:
-            return curr
+        sums = set([a + b for (a, b) in combinations(data[i - x:i], 2)])
+        if data[i] not in sums:
+            return data[i]
 
 
 def part2(data):
+    data = ints(data)
     hit = find(data)
-
-    data = [int(x) for x in data]
 
     for n in range(2, len(data)):
         for i in range(len(data), 0, -1):
