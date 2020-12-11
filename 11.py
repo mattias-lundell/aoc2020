37fbaac072a857fb54d05297f1142161cfbc031b
Mattias Lundell <mattias@fishbrain.com>
2020-12-11 22:07:28 +0100

    clean day 11 part 2 a bit

--- 11.py
+++ 11.py
@@ -65,52 +65,47 @@ def count_occupied_adjacent(seats, row, col):
 
 
 def part2(data):
-    seats = {}
-    for i in range(len(data)):
-        for j in range(len(data[0])):
-            seats[(i, j)] = data[i][j]
+    seats = [list(lines) for lines in data]
 
     cnt = 0
     while True:
-        next_gen = step2(seats, max(len(data), len(data[0])), cnt)
+        next_gen = step2(seats, max(len(data), len(data[0])))
         if next_gen == seats:
-            return count_occupied(seats.values())
+            return count_occupied(seats)
         seats = next_gen
         cnt += 1
 
 
-def step2(seats, ray_len, step):
-    next_gen = {}
-    for seat in seats:
-        cnt = count_adjacent_part2(seats, seat, ray_len, step)
-        if seats[seat] == 'L':
-            if cnt == 0:
-                next_gen[seat] = '#'
-            else:
-                next_gen[seat] = 'L'
-        elif seats[seat] == '#':
-            if cnt >= 5:
-                next_gen[seat] = 'L'
-            else:
-                next_gen[seat] = '#'
-        else:
-            next_gen[seat] = seats[seat]
+def step2(seats, ray_len):
+    next_gen = [list(rows) for rows in seats]
+    for row in range(len(seats)):
+        for col in range(len(seats[0])):
+            cnt = count_adjacent_part2(seats, row, col, ray_len)
+            if seats[row][col] == 'L':
+                if cnt == 0:
+                    next_gen[row][col] = '#'
+            elif seats[row][col] == '#':
+                if cnt >= 5:
+                    next_gen[row][col] = 'L'
     return next_gen
 
 
-def count_adjacent_part2(seats, seat, n_max, step):
-    (i, j) = seat
-
+def count_adjacent_part2(seats, row, col, n_max):
     cnt = 0
-    for di in [-1, 0, 1]:
-        for dj in [-1, 0, 1]:
-            if di == 0 and dj == 0:
+    rows = len(seats)
+    cols = len(seats[0])
+    for d_row in [-1, 0, 1]:
+        for d_col in [-1, 0, 1]:
+            if d_row == 0 and d_col == 0:
                 continue
             else:
                 for n in range(1, n_max):
-                    _i = i + n * di
-                    _j = j + n * dj
-                    _seat = seats.get((_i, _j), '.')
+                    _row = row + n * d_row
+                    _col = col + n * d_col
+                    if _row < 0 or _row >= rows or _col < 0 or _col >= cols:
+                        continue
+
+                    _seat = seats[_row][_col]
                     if _seat == '#':
                         cnt += 1
                         break
