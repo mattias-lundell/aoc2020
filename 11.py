eabaca17840a2737f7c03588c7bd91f650e192ea
Mattias Lundell <mattias@fishbrain.com>
2020-12-11 21:56:54 +0100

    clean day 11 part 1 a bit

--- 11.py
+++ 11.py
@@ -1,66 +1,67 @@
+from copy import deepcopy
+
+
 def part1(data):
-    seats = {}
-    for i in range(len(data)):
-        for j in range(len(data[0])):
-            seats[(i, j)] = data[i][j]
+    seats = [list(lines) for lines in data]
 
     cnt = 0
     while True:
         next_gen = step(seats)
         if next_gen == seats:
-            return count_occupied(seats.values())
+            return count_occupied(seats)
         seats = next_gen
         cnt += 1
 
+    return cnt
 
-def print_seats(seats, n_i, n_j):
-    for i in range(n_i):
-        row = []
-        for j in range(n_j):
-            row.append(seats[(i, j)])
+
+def print_seats(seats):
+    for row in seats:
         print(''.join(row))
     print()
 
 
 def step(seats):
-    next_gen = {}
-    for seat in seats:
-        cnt = count_occupied(adjacent(seats, seat))
-        if seats[seat] == 'L':
-            if cnt == 0:
-                next_gen[seat] = '#'
-            else:
-                next_gen[seat] = 'L'
-        elif seats[seat] == '#':
-            if cnt >= 4:
-                next_gen[seat] = 'L'
-            else:
-                next_gen[seat] = '#'
-        else:
-            next_gen[seat] = seats[seat]
+    next_gen = [list(rows) for rows in seats]
+    for row in range(len(seats)):
+        for col in range(len(seats[0])):
+            cnt = count_occupied_adjacent(seats, row, col)
+            seat = seats[row][col]
+            if seat == 'L':
+                if cnt == 0:
+                    next_gen[row][col] = '#'
+            elif seat == '#':
+                if cnt >= 4:
+                    next_gen[row][col] = 'L'
     return next_gen
 
 
-def count_occupied(xs):
+def count_occupied(seats):
     cnt = 0
-    for x in xs:
-        if x == '#':
-            cnt += 1
+    for row in seats:
+        for col in row:
+            if col == '#':
+                cnt += 1
 
     return cnt
 
 
-def adjacent(seats, seat):
-    (i, j) = seat
-    res = []
-    for di in [-1, 0, 1]:
-        for dj in [-1, 0, 1]:
-            if di == 0 and dj == 0:
+def count_occupied_adjacent(seats, row, col):
+    rows = len(seats)
+    cols = len(seats[0])
+    cnt = 0
+    for d_row in [-1, 0, 1]:
+        for d_col in [-1, 0, 1]:
+            if d_row == 0 and d_col == 0:
                 continue
             else:
-                res.append(seats.get((i + di, j + dj), '.'))
-
-    return res
+                _row = row + d_row
+                _col = col + d_col
+                if _row < 0 or _row >= rows or _col < 0 or _col >= cols:
+                    continue
+                if seats[_row][_col] == '#':
+                    cnt += 1
+    return cnt
 
 
 def part2(data):
@@ -72,7 +73,7 @@ def part2(data):
     cnt = 0
     while True:
         next_gen = step2(seats, max(len(data), len(data[0])), cnt)
-        if next_gen == seats:  # or cnt > 1:
+        if next_gen == seats:
             return count_occupied(seats.values())
         seats = next_gen
         cnt += 1
@@ -99,7 +100,8 @@ def step2(seats, ray_len, step):
 
 def count_adjacent_part2(seats, seat, n_max, step):
     (i, j) = seat
-    res = []
+
+    cnt = 0
     for di in [-1, 0, 1]:
         for dj in [-1, 0, 1]:
             if di == 0 and dj == 0:
@@ -109,9 +111,10 @@ def count_adjacent_part2(seats, seat, n_max, step):
                     _i = i + n * di
                     _j = j + n * dj
                     _seat = seats.get((_i, _j), '.')
-                    if _seat in ['#', 'L']:
-                        if _seat == '#':
-                            res.append('#')
+                    if _seat == '#':
+                        cnt += 1
+                        break
+                    elif _seat == 'L':
                         break
 
-    return len(res)
+    return cnt
