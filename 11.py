25dafcd8ca88f1db57d741bd4749a21a5c68c4b9
Mattias Lundell <mattias@fishbrain.com>
2020-12-12 21:11:41 +0100

    optimize 11

--- 11.py
+++ 11.py
@@ -1,3 +1,6 @@
+import os
+
+
 def part1(data):
     seats = [list(lines) for lines in data]
 
@@ -13,6 +16,7 @@ def part1(data):
 
 
 def print_seats(seats):
+    os.system('clear')
     for row in seats:
         print(''.join(row))
     print()
@@ -43,64 +47,105 @@ def count_occupied(seats):
     return cnt
 
 
-def count_occupied_adjacent(seats, row, col):
-    rows = len(seats)
-    cols = len(seats[0])
-    cnt = 0
+from functools import lru_cache
+
+
+@lru_cache(maxsize=None)
+def neighbours(row, col, rows, cols, distance=1):
+    res = []
     for [d_row, d_col] in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1),
                            (1, -1), (1, 0), (1, 1)]:
-        _row = row + d_row
-        _col = col + d_col
+        _row = row + d_row * distance
+        _col = col + d_col * distance
         if _row < 0 or _row >= rows or _col < 0 or _col >= cols:
             continue
+        res.append((_row, _col))
+    return res
+
+
+def count_occupied_adjacent(seats, row, col):
+    cnt = 0
+
+    rows = len(seats)
+    cols = len(seats[0])
+
+    for (_row, _col) in neighbours(row, col, rows, cols):
         if seats[_row][_col] == '#':
             cnt += 1
     return cnt
 
 
 def part2(data):
-    seats = [list(lines) for lines in data]
+
+    seats = {}
+
+    for i, rows in enumerate(data):
+        for j, c in enumerate(rows):
+            if c in "#L":
+                seats[(i, j)] = c
+
+    rows = len(data)
+    cols = len(data[0])
 
     cnt = 0
     while True:
-        next_gen = step2(seats, max(len(data), len(data[0])))
-        if next_gen == seats:
-            return count_occupied(seats)
+        modified, next_gen = step2(seats, max(rows, cols), rows, cols)
+        if not modified:
+            return len([x for x in seats.values() if x == '#'])
         seats = next_gen
         cnt += 1
 
 
-def step2(seats, ray_len):
-    next_gen = [list(rows) for rows in seats]
-    for row in range(len(seats)):
-        for col in range(len(seats[0])):
-            cnt = count_adjacent_part2(seats, row, col, ray_len)
-            if seats[row][col] == 'L':
-                if cnt == 0:
-                    next_gen[row][col] = '#'
-            elif seats[row][col] == '#':
-                if cnt >= 5:
-                    next_gen[row][col] = 'L'
-    return next_gen
+def step2(seats, ray_len, rows, cols):
+    swaps = []
+    modified = False
+    for seat in seats:
+        cnt = count_adjacent_part2(seats, seat, ray_len, rows, cols)
+        _seat = seats[seat]
+        if _seat == 'L' and cnt == 0:
+            swaps.append(seat)
+        elif _seat == '#' and cnt >= 5:
+            swaps.append(seat)
+
+    if len(swaps) > 0:
+        modified = True
+        for swap in swaps:
+            seats[swap] = _swap[seats[swap]]
+
+    return modified, seats
 
 
-def count_adjacent_part2(seats, row, col, n_max):
-    cnt = 0
-    rows = len(seats)
-    cols = len(seats[0])
+_swap = {
+    '#': 'L',
+    'L': '#',
+}
+
+_n2_cache = {}
+
+
+def neighbours2(seats, seat, rows, cols, distance=100):
+    if (seat, distance) in _n2_cache:
+        return _n2_cache[(seat, distance)]
+
+    res = []
+    (row, col) = seat
     for [d_row, d_col] in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1),
                            (1, -1), (1, 0), (1, 1)]:
-        for n in range(1, n_max):
-            _row = row + n * d_row
-            _col = col + n * d_col
+        for d in range(1, distance + 1):
+            _row = row + d_row * d
+            _col = col + d_col * d
             if _row < 0 or _row >= rows or _col < 0 or _col >= cols:
                 break
-
-            _seat = seats[_row][_col]
-            if _seat == '#':
-                cnt += 1
-                break
-            elif _seat == 'L':
+            elif (_row, _col) in seats:
+                res.append((_row, _col))
                 break
 
-    return cnt
+    _n2_cache[seat] = res
+    return res
+
+
+def count_adjacent_part2(seats, seat, n_max, rows, cols):
+    return len([
+        seat for seat in neighbours2(seats, seat, rows, cols)
+        if seats[seat] == '#'
+    ])
